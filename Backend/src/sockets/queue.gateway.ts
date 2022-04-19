943a5c2005eb22f97ca811ab6b88526c5467e54c
Dmitry Shevchuk <sheva2dim@gmail.com>
2022-04-19 23:26:59 +0300

    Connect socket and create queue

---
+++ Backend/src/sockets/queue.gateway.ts
@@ -0,0 +1,53 @@
+import { Logger } from '@nestjs/common'
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer
+} from '@nestjs/websockets'
+import { Server, Socket } from 'socket.io'
+import { regToQueueDto, userQueueDto } from './dto/queue.dto'
+
+@WebSocketGateway()
+export class QueueGateway {
+  private queue = []
+  private logger = new Logger(QueueGateway.name)
+
+  @WebSocketServer()
+  server: Server
+
+  @SubscribeMessage('/search/room')
+  registerToQueue(client: Socket, payload: regToQueueDto) {
+    const data: userQueueDto = {
+      socket: client.id,
+      ...payload
+    }
+
+    const findColor: string[] = this.getFindsColors(data.color)
+
+    const anotherPlayer: userQueueDto = this.getUserByColor(findColor)
+
+    if (!anotherPlayer && !this.getUserBySocket(data)) {
+      this.queue.push(data)
+    } else {
+      const index = this.queue.findIndex((obj) => !!this.getUserBySocket(obj))
+      if (index >= 0) {
+        this.queue.splice(index, 1)
+      }
+    }
+  }
+
+  getFindsColors(colors: string[]): string[] {
+    return colors.map(color => {
+      if (color === 'white' ) return 'black'
+      if (color === 'black' ) return 'white'
+    })
+  }
+
+  getUserByColor(colors: string[]): userQueueDto {
+    return this.queue.find((user) => colors.some((color) => user.color.includes(color)))
+  }
+
+  getUserBySocket(data: userQueueDto): userQueueDto {
+    return this.queue.find(obj => Object.is(obj.socket, data.socket))
+  }
+}
