a964d1360d4ee1bed63bcbe5938c5f286b943ac1
Dmitry Shevchuk <sheva2dim@gmail.com>
2022-04-20 16:31:42 +0300

    Add static

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { SocketsModule } from './sockets/sockets.module'
 
 @Module({
-  imports: [SocketsModule],
+  imports: [
+    SocketsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../', 'client'),
+      exclude: ['/api*'],
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
