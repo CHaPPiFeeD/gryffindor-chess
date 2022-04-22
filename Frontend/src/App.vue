09fb61ebb5549e40ea9258da96f5e80b909b9fbb
Dmitry Shevchuk <sheva2dim@gmail.com>
2022-04-22 13:56:49 +0300

    Fix frontend

---
+++ Frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <MainPage />
+</template>
+
+<script>
+import MainPage from "./components/MainPage.vue";
+export default {
+  name: "App",
+  components: {
+    MainPage,
+  },
+};
+</script>
+
+<style>
+body {
+  background-image: url("assets/bcg_Img.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
